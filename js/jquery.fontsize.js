b3d6e2d307feaa4205558c8fbf39dedb0d43523d
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-26 09:00:17 +0000

    Tag 1.4.2
    git-svn-id: https://plugins.svn.wordpress.org/zeno-font-resizer/tags/1.4.2@1167579 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/jquery.fontsize.js
@@ -0,0 +1,59 @@
+
+
+jQuery.fn.zeno_font_resizer_manager = function () {
+	var zeno_font_resizer_value = jQuery('#zeno_font_resizer_value').val();
+	var zeno_font_resizer_ownid = jQuery('#zeno_font_resizer_ownid').val();
+	var zeno_font_resizer_ownelement = jQuery('#zeno_font_resizer_ownelement').val();
+	var zeno_font_resizer_resizeSteps = jQuery('#zeno_font_resizer_resizeSteps').val();
+	var zeno_font_resizer_cookieTime = jQuery('#zeno_font_resizer_cookieTime').val();
+	var zeno_font_resizer_element = zeno_font_resizer_value;
+
+	if (zeno_font_resizer_value == "innerbody") {
+		zeno_font_resizer_element = "div#innerbody";
+	} else if(zeno_font_resizer_value == "ownid") {
+		zeno_font_resizer_element = "div#" + zeno_font_resizer_ownid;
+	} else if(zeno_font_resizer_value == "ownelement") {
+		zeno_font_resizer_element = zeno_font_resizer_ownelement;
+	}
+
+	var startFontSize = parseFloat(jQuery(zeno_font_resizer_element + "").css("font-size"));
+	var savedSize = Cookies.get('fontSize');
+
+	if (savedSize > 4) {
+		jQuery(zeno_font_resizer_element).css("font-size", savedSize + "px");
+	}
+
+	jQuery('.zeno_font_resizer_add'  ).css("cursor","pointer");
+	jQuery('.zeno_font_resizer_minus').css("cursor","pointer");
+	jQuery('.zeno_font_resizer_reset').css("cursor","pointer");
+
+	jQuery('.zeno_font_resizer_add').click(function() {
+		var newFontSize = parseFloat(jQuery(zeno_font_resizer_element + "").css("font-size"));
+		newFontSize = newFontSize + parseFloat(zeno_font_resizer_resizeSteps);
+		var maxFontSize = startFontSize + ( zeno_font_resizer_resizeSteps * 5 );
+		if (newFontSize > maxFontSize) { return; }
+		jQuery(zeno_font_resizer_element + "").css("font-size", newFontSize + "px");
+		Cookies.set('fontSize', newFontSize, {expires: parseInt(zeno_font_resizer_cookieTime), path: '/'});
+		return false;
+	});
+	jQuery('.zeno_font_resizer_minus').click(function() {
+		var newFontSize = parseFloat(jQuery(zeno_font_resizer_element + "").css("font-size"))
+		newFontSize = newFontSize - zeno_font_resizer_resizeSteps;
+		var minFontSize = startFontSize - ( zeno_font_resizer_resizeSteps * 5 );
+		if (newFontSize < minFontSize) { return; }
+		jQuery("" + zeno_font_resizer_element + "").css("font-size", newFontSize + "px");
+		Cookies.set('fontSize', newFontSize, {expires: parseInt(zeno_font_resizer_cookieTime), path: '/'});
+		return false;
+	});
+	jQuery('.zeno_font_resizer_reset').click(function() {
+		jQuery("" + zeno_font_resizer_element + "").css("font-size", startFontSize);
+		Cookies.set('fontSize', startFontSize, {expires: parseInt(zeno_font_resizer_cookieTime), path: '/'});
+		return false;
+	});
+}
+
+
+jQuery(document).ready(function(){
+	jQuery(".zeno_font_resizer").zeno_font_resizer_manager();
+});
+
